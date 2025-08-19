32a35c16d406119308c077e6cb4641e0b79bc8c6
July <20288016+AliceNEET@users.noreply.github.com>
2025-08-19 17:29:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,17 @@
 '''
 Author: JY
 Date: 2023-12-27 18:30:32
-LastEditTime: 2023-12-27 21:02:08
+LastEditTime: 2025-08-19 16:56:50
 LastEditors: JY
 Description: 
 FilePath: /Ablesci-checkin/main.py
 '''
-#!/usr/bin/env python
-# coding=utf-8
+
 import requests
 import os
 import json
-env = os.environ.get("CONFIG")
+
+
 
 
 def ablesci(headers):
@@ -24,6 +24,10 @@ def ablesci(headers):
 
 
 if __name__ == "__main__":
+    env = os.environ.get("CONFIG")
+    if env is None:
+        env = '{}'
+
     config_path = os.path.join(os.path.dirname(__file__), "config.json")
     if os.path.exists(config_path):
         with open(config_path, "r", encoding="utf-8") as f:
@@ -31,29 +35,27 @@ if __name__ == "__main__":
     else:
         config = json.loads(env)
     
-
-    _check_item = config.get("ablesci", [])[0]
     pushToken = config.get("PUSHTOKEN", [])
-    
 
-    Cookie = _check_item.get("cookie",[])
-    headers = {
-        "Accept": "application/json, text/javascript, */*; q=0.01",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "zh-CN,zh;q=0.9",
-        "Cookie": f"{Cookie}",
-        "DNT": "1",
-        "Referer": "https://www.ablesci.com/",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-origin",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
-        "X-Requested-With": "XMLHttpRequest"
-    }
+    for _check_item in config.get("ablesci", []):
 
-    if pushToken != None:
-        msg = ablesci(headers=headers)
-        content = "今日科研通签到！\n" + str(msg)
-        requests.post('https://www.pushplus.plus/send', { 'token': pushToken, 'title': '科研通签到', 'content': content });
+        Cookie = _check_item.get("cookie",[])
+        headers = {
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "zh-CN,zh;q=0.9",
+            "Cookie": f"{Cookie}",
+            "DNT": "1",
+            "Referer": "https://www.ablesci.com/",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+            "X-Requested-With": "XMLHttpRequest"
+        }
 
+        if pushToken != None:
+            msg = ablesci(headers=headers)
+            content = "今日科研通签到！\n" + str(msg)
+            requests.post('https://www.pushplus.plus/send', { 'token': pushToken, 'title': '科研通签到', 'content': content })
 
